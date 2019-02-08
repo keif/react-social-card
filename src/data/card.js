eebaea5257bddfc38e9878a559033c171687eb78
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-02-08 00:30:08 -0500

    Create Message component and card data

---
+++ src/data/card.js
@@ -0,0 +1,13 @@
+const card = {
+	headline: "Learning React? Start Small.",
+
+	name: "Rick Sanchez",
+
+	message: "Can't pry yourself away from tutorials? The cure is to make tiny little experiment apps.",
+
+	url: "https://baker.af",
+
+	urlDisplayName: "baker.af"
+};
+
+export default card;
